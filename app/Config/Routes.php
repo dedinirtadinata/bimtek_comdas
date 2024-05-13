d35fe7bee718bd07db6c6d6a5d55f16e329562eb
dedinirtadinata <s@dedinirtadinata.net>
2024-05-13 21:29:50 +0700

    init halaman beranda

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/beranda/dukungan', 'Home::dukungan');
+$routes->get('/beranda/tentang', 'Home::tentang');
+$routes->get('/beranda/ulasan', 'Home::ulasan');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,27 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        return view('beranda_dukungan');
+    }
+
+    public function dukungan() : string
+    {
+        return view('beranda_dukungan');
+    }
+
+    public function tentang() : string
+    {
+        // return view('beranda_dukungan');
+        return "ini adalah halaman tentang";
+    }
+
+    public function katalog() : string
+    {
+        // return view('beranda_dukungan');
+    }
+
+    public function hitung($a, $b) : int
+    {
+        return $a+$b;
     }
 }
